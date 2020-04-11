77eff3e68892adbcc959e046a8150006390db591
Esther Spiteri <esther.spiteri.18@um.edu.mt>
2020-04-11 18:47:38 +0200

    Implemented functionality for setMapSize. Its success is determined by the minimum map size of 5.

--- src/main/Part1/Map.java
+++ src/main/Part1/Map.java
@@ -2,7 +2,16 @@ package Part1;
 
 public class Map {
 
+    private int size;
+
     public boolean setMapSize(int size) {
-        return true;
+        //if inputted size is less than minimum of 5
+        if(size < 5){
+            return false;
+        }
+        else{
+            this.size = size;
+            return true;
+        }
     }
 }
