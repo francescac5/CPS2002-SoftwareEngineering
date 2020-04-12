d515b75df99be477c631d1004c3cab21ba15c087
francescac5 <francesca.chircop.18@um.edu.mt>
2020-04-12 10:54:41 +0200

    Implemented the variables for the x and y axis together with their respective getters and setters

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,2 +1,24 @@
 public class Position {
+
+    //variables for the x-axis and y-axis
+    private int x;
+    private int y;
+
+    //getters and setters for the x-axis and y-axis
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
 }
