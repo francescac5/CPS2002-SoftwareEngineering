dc8c0d61821cebae5d4e9bb41478a2f8a8871056
Esther Spiteri <esther.spiteri.18@um.edu.mt>
2020-04-12 09:04:14 +0200

    Implemented function to initialize the mapCount to zero. Added getter for MapCount for testing purposes. Added more tests for generate to create multiple html files.

--- src/main/Part1/Map.java
+++ src/main/Part1/Map.java
@@ -1,8 +1,21 @@
 package Part1;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Map {
 
     private int size;
+    private static int mapCount;
+
+    public void initMapCount(){
+        mapCount = 0;
+    }
+
+    //only for testing purposes
+    public int getMapCount() {
+        return mapCount;
+    }
 
     public boolean setMapSize(int size) {
         //if inputted size is less than minimum of 5
@@ -16,5 +29,17 @@ public class Map {
     }
 
     public void generate() {
+        try {
+            File mapFile = new File("src\\GameMaps\\map"+mapCount+".html");
+            if (mapFile.createNewFile()) {
+                System.out.println("File created: " + mapFile.getName());
+                mapCount++;
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
--- src/test/Part1/MapTests.java
+++ src/test/Part1/MapTests.java
@@ -13,12 +13,28 @@ public class MapTests {
     @Before
     public void setup() {
         map = new Map();
+
+        //reset map count to zero
+        map.initMapCount();
     }
 
     @After
     public void tearDown() {
+
         map = null;
     }
+//********** map.initMapCount() tests *******\\
+
+    @Test
+    public void testInitMapCount(){
+        //Exercise
+        map.initMapCount();
+
+        //Assert
+        assertEquals(0, map.getMapCount());
+    }
+
+//********** map.setMapSize() tests **********\\
 
     @Test
     public void testSetMapSize_GreaterThanMinSize() {
@@ -70,16 +86,200 @@ public class MapTests {
         assertFalse(result);
     }
 
+//********** map.generate() tests **********\\
     @Test
     public void testGenerate() {
         //Exercise
         //generates map0.html file
         map.generate();
 
-        File map0 = new File("CPS2002-SoftwareEngineering\\src\\main\\resources\\map0.html");
+        File map0 = new File("src\\GameMaps\\map0.html");
 
         //Assert
         assertTrue(map0.exists());
+        assertEquals(1, map.getMapCount());
+
+        //deleting generated map
+        assertTrue(map0.delete());
     }
 
+    @Test
+    public void testGenerateTwoMaps_MinPlayers() {
+        //Exercise
+        //generates map0.html file
+        map.generate();
+
+        File map0 = new File("src\\GameMaps\\map0.html");
+
+        //Assert
+        assertTrue(map0.exists());
+        assertEquals(1, map.getMapCount());
+
+        //generates map1.html file
+        map.generate();
+
+        File map1 = new File("src\\GameMaps\\map1.html");
+
+        //Assert map0.html and map1.html exist
+        assertTrue(map0.exists());
+        assertTrue(map1.exists());
+        assertEquals(2, map.getMapCount());
+
+        //deleting generated maps
+        assertTrue(map0.delete());
+        assertTrue(map1.delete());
+    }
+
+    @Test
+    public void testGenerateThreeMaps() {
+        //Exercise
+        //generates map0.html file
+        map.generate();
+
+        File map0 = new File("src\\GameMaps\\map0.html");
+
+        //Assert
+        assertTrue(map0.exists());
+        assertEquals(1, map.getMapCount());
+
+        //generates map1.html file
+        map.generate();
+
+        File map1 = new File("src\\GameMaps\\map1.html");
+
+        //Assert map0.html and map1.html exist
+        assertTrue(map0.exists());
+        assertTrue(map1.exists());
+        assertEquals(2, map.getMapCount());
+
+        //generates map2.html file
+        map.generate();
+
+        File map2 = new File("src\\GameMaps\\map2.html");
+
+        //Assert map0.html, map1.html and map2.html exist
+        assertTrue(map0.exists());
+        assertTrue(map1.exists());
+        assertTrue(map2.exists());
+        assertEquals(3, map.getMapCount());
+
+        //deleting generated maps
+        assertTrue(map0.delete());
+        assertTrue(map1.delete());
+        assertTrue(map2.delete());
+    }
+
+    @Test
+    public void testGenerateEightMaps_MaxPlayers() {
+        //Exercise
+        //generates map0.html file
+        map.generate();
+
+        File map0 = new File("src\\GameMaps\\map0.html");
+
+        //Assert
+        assertTrue(map0.exists());
+        assertEquals(1, map.getMapCount());
+
+        //generates map1.html file
+        map.generate();
+
+        File map1 = new File("src\\GameMaps\\map1.html");
+
+        //Assert map0.html and map1.html exist
+        assertTrue(map0.exists());
+        assertTrue(map1.exists());
+        assertEquals(2, map.getMapCount());
+
+        //generates map2.html file
+        map.generate();
+
+        File map2 = new File("src\\GameMaps\\map2.html");
+
+        //Assert map0.html, map1.html and map2.html exist
+        assertTrue(map0.exists());
+        assertTrue(map1.exists());
+        assertTrue(map2.exists());
+        assertEquals(3, map.getMapCount());
+
+        //generates map3.html file
+        map.generate();
+
+        File map3 = new File("src\\GameMaps\\map3.html");
+
+        //Assert map0.html, map1.html, map2.html and map3.html exist
+        assertTrue(map0.exists());
+        assertTrue(map1.exists());
+        assertTrue(map2.exists());
+        assertTrue(map3.exists());
+        assertEquals(4, map.getMapCount());
+
+        //generates map4.html file
+        map.generate();
+
+        File map4 = new File("src\\GameMaps\\map4.html");
+
+        //Assert map0.html, map1.html map2.html, map3.html and map4.html exist
+        assertTrue(map0.exists());
+        assertTrue(map1.exists());
+        assertTrue(map2.exists());
+        assertTrue(map3.exists());
+        assertTrue(map4.exists());
+        assertEquals(5, map.getMapCount());
+
+        //generates map5.html file
+        map.generate();
+
+        File map5 = new File("src\\GameMaps\\map5.html");
+
+        //Assert map0.html, map1.html map2.html, map3.html, map4.html and map5.html exist
+        assertTrue(map0.exists());
+        assertTrue(map1.exists());
+        assertTrue(map2.exists());
+        assertTrue(map3.exists());
+        assertTrue(map4.exists());
+        assertTrue(map5.exists());
+        assertEquals(6, map.getMapCount());
+
+        //generates map6.html file
+        map.generate();
+
+        File map6 = new File("src\\GameMaps\\map6.html");
+
+        //Assert map0.html, map1.html map2.html, map3.html, map4.html, map5.html and map6.html exist
+        assertTrue(map0.exists());
+        assertTrue(map1.exists());
+        assertTrue(map2.exists());
+        assertTrue(map3.exists());
+        assertTrue(map4.exists());
+        assertTrue(map5.exists());
+        assertTrue(map6.exists());
+        assertEquals(7, map.getMapCount());
+
+        //generates map7.html file
+        map.generate();
+
+        File map7 = new File("src\\GameMaps\\map7.html");
+
+        //Assert map0.html, map1.html map2.html, map3.html, map4.html, map5.html, map6.html and map7.html exist
+        assertTrue(map0.exists());
+        assertTrue(map1.exists());
+        assertTrue(map2.exists());
+        assertTrue(map3.exists());
+        assertTrue(map4.exists());
+        assertTrue(map5.exists());
+        assertTrue(map6.exists());
+        assertTrue(map7.exists());
+        assertEquals(8, map.getMapCount());
+
+        //deleting generated maps
+        assertTrue(map0.delete());
+        assertTrue(map1.delete());
+        assertTrue(map2.delete());
+        assertTrue(map3.delete());
+        assertTrue(map4.delete());
+        assertTrue(map5.delete());
+        assertTrue(map6.delete());
+        assertTrue(map7.delete());
+    }
 }
