ef21e9a2bbaec7bd8b1ed98aae1fdd4d99362196
francescac5 <francesca.chircop.18@um.edu.mt>
2020-04-12 12:17:36 +0200

    Implemented testSetY

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -9,4 +9,12 @@ public class PositionTest {
         p.setX(2);
         assertTrue(p.getX() == 2);
     }
+
+    @Test
+    public void testSetY(){
+        Position p = new Position();
+        p.setY(3);
+        assertTrue(p.getY() == 3);
+    }
+
 }
